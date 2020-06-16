10d866200b28cb45799a2e075cccc4e89487ed5e
Hyung1Jung <HyungilJung94@gmail.com>
2020-06-17 06:40:13 +0900

    seminar2

--- homework2/seminar2/fs_sync.js
+++ homework2/seminar2/fs_sync.js
@@ -6,6 +6,7 @@ const numArr = [1, 2, 3, 4, 5];
    fs.writeFileSync ( file, data, [options]) {}
    동기 방식으로 파일 쓰기 - 순서 확인해보기
 */
+
 numArr.forEach((num) => {
    const title = 'sync' + num;
    const data = `파일이 잘 만들어 졌어요!\n제 이름은 '${title}.txt'입니다 ~ `;
