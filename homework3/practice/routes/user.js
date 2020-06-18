5b4385b3e90d87ed2e303b40643079dbf1fb14ae
Hyung1Jung <HyungilJung94@gmail.com>
2020-06-18 18:59:05 +0900

    seminar3

---
+++ homework3/practice/routes/user.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+const User = require('../models/user');
+let util = require('../modules/util');
+// let statusCode = require('../modules/statusCode');
+// let resMessage = require('../modules/responseMessage');
+
+/* 
+    ✔️ sign up
+    METHOD : POST
+    URI : localhost:3000/user/signup
+    REQUEST BODY : id, name, password, email
+    RESPONSE STATUS : 200 (OK)
+    RESPONSE DATA : User ID
+*/
+
+// 1단계
+// router.post('/signup', async (req, res) => {
+//     const { id, name, password, email } = req.body;
+//     User.push({id, name, password, email});
+//     res.status(200).send(User);
+// });
+ 
+// 2단계
+router.post('/signup', async (req, res) => {
+    const { id, name, password, email } = req.body;
+    // request data 확인 - 없다면 Bad Request 반환
+    if ( !id || !name || !password || !email ) {
+        return res.status(400).send({ message: 'BAD REQUEST' });
+    }
+    //already ID
+    if (User.filter(user => user.id == id).length > 0) {
+        return res.status(400).send({ message: 'ALREADY ID' });
+    }
+    User.push({id, name, password, email});
+    res.status(200).send(util.success(200, '회원가입 성공', { userId : id }));
+});
+
+module.exports = router;
